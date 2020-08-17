1e1194e770677c1bf350089f4b4744ed5b10bc04
Pormes <66345108+pormes@users.noreply.github.com>
2020-08-17 21:12:52 +0700

    Add files via upload

---
+++ anonymous.sh
@@ -0,0 +1,143 @@
+echo ""
+echo ""
+echo ""
+echo "ARE YOU READY FOR START NOW"
+delay 5
+echo ""
+echo ""
+echo ""
+echo "LET'S SUBSCRIBE PORMES17"
+delay 5
+echo ""
+echo ""
+echo ""
+echo "WAITING FOR STARTED"
+delay 5
+echo ""
+echo ""
+echo ""
+
+pkg install && pkg update && pkg upgrade
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+pkg install science-repo -y
+pkg install game-repo -y
+pkg install clang -y
+pkg install vlang -y
+pkg install slang -y
+pkg install golang -y
+pkg install python -y
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install apache2 -y
+pkg install curl -y
+pkg install openssh -y
+pkg install openssl -y
+pkg install grep -y
+pkg install wget -y
+pkg install rust -y
+pkg install imagemagick -y
+pkg install nano  -y
+pkg install cowsay -y
+pkg install figlet -y
+pkg install toilet -y
+pkg install screenfetch -y
+pkg install aria2 -y
+pkg install tree -y
+pkg install busybox -y
+pkg install tsu -y
+pkg install man -y
+pkg install postgresql -y
+pkg install hydra -y
+pkg install nmap -y
+pkg install sqlmap -y
+pkg install at -y
+pkg install binutils -y
+pkg install coreutils -y
+pkg install dnsutils -y
+pkg install tracepath -y
+pkg install tor -y
+pkg install nodejs-lts -y
+pkg install php -y
+pkg install pkg-config -y
+pkg install automake -y
+pkg install asciinema -y
+pkg install asciidoc -y
+pkg install kona -y
+pkg install o-editor -y
+pkg install silversearcher-ag -y
+pkg install termux-toold -y
+pkg install vim -y
+pkg install tinyfugue -y
+pkg install gh -y
+pkg install ghostscript -y
+pkg install graphicsmagick -y
+pkg install graphviz -y
+pkg install mg -y
+pkg install ripgrep -y
+pkg install wireguard-tools -y
+pkg install dash -y
+pkg install i3 -y
+pkg install iw -y
+pkg install ruby-ri -y
+pkg install jo -y
+pkg install jq -y
+pkg install mtools -y
+pkg install sl -y
+pkg install util-linux -y
+pkg install m4 -y
+pkg install mc -y
+pkg install mg -y
+pkg install mu -y
+pkg install myrepos -y
+pkg install ne -y
+pkg install nushell -y
+pkg install tin-summer -y
+pkg install procps -y
+pkg install pv -y
+pkg install radare2 -y
+pkg install surfraw -y
+pkg install st -y
+pkg install runit -y
+pkg install pastebinit -y
+pkg install task-spooler -y
+pkg install virustotal-cli -y
+pkg install xz-utils -y
+pkg install p7zip -y
+pkg install zip -y
+pkg install unzip -y
+pkg install termux-am -y
+pkg install bc -y
+pkg install netcat -y
+pkg install rcs -y
+pkg install rcshell -y
+pkg install tigervnc -y
+pkg install sc -y
+pkg install dx -y
+pkg install ed -y
+pkg install fd -y
+pkg install gatling -y
+pkg install texlive-bin -y
+pkg install tshark -y
+pkg install bash -y
+pkg install fish -y
+pkg install axel -y
+pkg install fish -y
+pkg install electrum -y
+pkg install metasploit -y
+
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo ""
+echo ""
+echo ""
+echo "ARE YOU PRO"
+echo ""
+echo ""
+echo "SUBSCRIBE PORMES17"
+exit
